524c8e1ee51acf316446ac8f9b74abc2b520ecd7
reggietenk <reggietenkorang@gmail.com>
2022-04-12 18:42:21 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from '../src/components/Nav'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+       <main>
+      </main>
+    </div>
+  );
+}
+
+export default App;
